2f15cef5ee62b546b4bbb7c5096119f48b1568a8
Viggo <viggo.hermansson2@gmail.com>
2025-09-26 02:06:36 +0200

    Added question 5

--- q5/register_file.sv
+++ q5/register_file.sv
@@ -1,13 +1,43 @@
-module registerfile (input logic clk,
-                     input logic rst_n,
-                     input logic write_en,
-                     input logic [3:0] write_addr,
-                     input logic [7:0] data_in,
-                     input logic [3:0] read_addr1,
-                     input logic [3:0] read_addr2,
-                     output logic [7:0] data_out1,
-                     output logic [7:0] data_out2
-                     );
-    // complete here
+module registerfile (
+    input logic clk,
+    input logic rst_n,
+    input logic write_en,
+    input logic [3:0] write_addr,
+    input logic [7:0] data_in,
+    input logic [3:0] read_addr1,
+    input logic [3:0] read_addr2,
+    output logic [7:0] data_out1,
+    output logic [7:0] data_out2
+);
+
+    // this is a register file with a width of 8 bits and a depth of 16 registers
+    // it has an asynchronous negative reset
+    // it has a write-enable signal and one synchronous write input
+    // it has two synchronous read outputs, each with their own respective read address inputs
+
+    // a register file is an array of flip-flops
+    // the width is defined first and the depth second
+    logic [15:0][7:0] reg_file;
+
+    // the main always_ff procedure should trigger both on clock and on reset
+    always_ff @(posedge clk or negedge rst_n) begin
+        // we reset the contents to the reg file if the rst_n signal is disabled (low)
+        // this behavior is asynchrous, as it is triggered by rst_n rather than clk
+        if(~rst_n) begin
+            for (int i = 0; i < 16; i++) begin
+                reg_file <= '0;
+            end
+        // we may only write if the write_en signal is enabled (high)
+        end else if (write_en) begin
+            reg_file[write_addr] <= data_in;
+        end
+    end
+
+    // the specifications say that "the read is synchronized with the clock",
+    // so we add flip-flops to the output to achieve this behavior
+    always_ff @(posedge clk) begin
+        data_out1 <= reg_file[read_addr1];
+        data_out2 <= reg_file[read_addr2];
+    end
 
 endmodule
--- q5/register_file_tb.sv
+++ q5/register_file_tb.sv
@@ -1,5 +1,137 @@
 module registerfile_tb;
 
-// complete here
+    logic clk;
+    logic rst_n;
+    logic write_en;
+    logic [3:0] write_addr;
+    logic [7:0] data_in;
+    logic [3:0] read_addr1;
+    logic [3:0] read_addr2;
+    logic [7:0] data_out1;
+    logic [7:0] data_out2;
+
+    registerfile reg_file(
+        .clk(clk),
+        .rst_n(rst_n),
+        .write_en(write_en),
+        .write_addr(write_addr),
+        .data_in(data_in),
+        .read_addr1(read_addr1),
+        .read_addr2(read_addr2),
+        .data_out1(data_out1),
+        .data_out2(data_out2)
+    );
+
+    initial begin
+        // initial state
+        clk        = 0;
+        rst_n      = 0;
+        write_en   = 0;
+        write_addr = 0000;
+        data_in    = 00000000;
+        read_addr1 = 0000;
+        read_addr2 = 0000;
+        #10ns;
+
+        // setup a write, but don't write yet (no write_en)
+        // we should not see an output
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 0;
+        write_addr = 0001;
+        data_in    = 11111111;
+        read_addr1 = 0001;
+        read_addr2 = 0010;
+        #10ns;
+        clk = 0;
+        #10ns;
+
+        // write
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 1;
+        write_addr = 0010;
+        data_in    = 11111111;
+        read_addr1 = 0001;
+        read_addr2 = 0010;
+        #10ns;
+        clk = 0;
+        #10ns;
+
+        // write another
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 1;
+        write_addr = 0001;
+        data_in    = 00001111;
+        read_addr1 = 0001;
+        read_addr2 = 0010;
+        #10ns;
+        clk = 0;
+        #10ns;
+
+        // override write
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 1;
+        write_addr = 0010;
+        data_in    = 00110011;
+        read_addr1 = 0001;
+        read_addr2 = 0010;
+        #10ns;
+        clk = 0;
+        #10ns;
+
+        // read empty
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 0;
+        write_addr = 0010;
+        data_in    = 00110011;
+        read_addr1 = 0010;
+        read_addr2 = 1100;
+        #10ns;
+        clk = '0;
+        #10ns;
+
+        // write again
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 1;
+        write_addr = 1100;
+        data_in    = 00110011;
+        read_addr1 = 0010;
+        read_addr2 = 1100;
+        #10ns;
+        clk = '0;
+        #10ns;
+
+        // reset
+        clk        = 1;
+        rst_n      = 0;
+        write_en   = 0;
+        write_addr = 0010;
+        data_in    = 00110011;
+        read_addr1 = 0001;
+        read_addr2 = 0010;
+        #10ns;
+        clk = 0;
+        #10ns;
+
+        // no write
+        clk        = 1;
+        rst_n      = 1;
+        write_en   = 0;
+        write_addr = 0010;
+        data_in    = 00110011;
+        read_addr1 = 0001;
+        read_addr2 = 0010;
+        #10ns;
+        clk = 0;
+        #10ns;
+
+        // done
+        $stop;
+    end
 
 endmodule
